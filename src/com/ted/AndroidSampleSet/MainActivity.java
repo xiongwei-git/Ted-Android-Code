abe7958f4ceacc750e60b6b98e97917f9e88e320
Ted <xiong-wei@hotmail.com>
2014-12-25 02:02:37 +0800

    add libs

--- src/com/ted/AndroidSampleSet/MainActivity.java
+++ src/com/ted/AndroidSampleSet/MainActivity.java
@@ -2,14 +2,69 @@ package com.ted.AndroidSampleSet;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+import java.util.ArrayList;
+
+public class MainActivity extends Activity implements SwipeRefreshLayout.OnRefreshListener {
+
+    private SwipeRefreshLayout mSwipeLayout;
+    private ListView mListView;
+    private ArrayList<String> list = new ArrayList<String>();
+
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        mListView = (ListView) findViewById(R.id.listview);
+        mListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                getData()));
+
+        mSwipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
+        mSwipeLayout.setOnRefreshListener(this);
+        mSwipeLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light, android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+    }
+
+    private ArrayList<String> getData() {
+        list.add("Hello");
+        list.add("This is stormzhang");
+        list.add("An Android Developer");
+        list.add("Love Open Source");
+        list.add("My GitHub: stormzhang");
+        list.add("weibo: googdev");
+
+        list.add("Love Open Source");
+        list.add("My GitHub: stormzhang");
+        list.add("weibo: googdev");
+        list.add("Love Open Source");
+        list.add("My GitHub: stormzhang");
+        list.add("weibo: googdev");
+        list.add("Love Open Source");
+        list.add("My GitHub: stormzhang");
+        list.add("weibo: googdev");
+        list.add("Love Open Source");
+        list.add("My GitHub: stormzhang");
+        list.add("weibo: googdev");
+        list.add("Love Open Source");
+        list.add("My GitHub: stormzhang");
+        list.add("weibo: googdev");
+        list.add("Love Open Source");
+        list.add("My GitHub: stormzhang");
+        list.add("weibo: googdev");
+        return list;
+    }
+
+    public void onRefresh() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mSwipeLayout.setRefreshing(false);
+            }
+        }, 5000);
     }
 }
